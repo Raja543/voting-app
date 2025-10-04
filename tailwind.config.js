a09d5b230c8e64026aca0b62db6e62e2251d342c
Raja543 <rajakumarmahto952@gmail.com>
2025-10-04 19:00:53 +0530

    fixed css loading issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}', 
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'Segoe UI',
-          'Roboto',
-          'Oxygen',
-          'Ubuntu',
-          'Cantarell',
-          'sans-serif',
-        ],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-  corePlugins: {
-    float: false,
-    clear: false,
-    skew: false,
-    caretColor: false,
-    sepia: false,
-  },
 };
